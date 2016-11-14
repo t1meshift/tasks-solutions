4d9349d4d525c32bb59a0d3b0228efd5659ca783
t1meshift <me@t1meshift.cz.cc>
2016-11-15 00:07:16 +1000

    Increment and jump
    https://imcs.dvfu.ru/cats/static/problem_text-cpid-1092004.html

---
+++ inc_and_jmp.cpp
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+using namespace std;
+
+char a;
+int add_cnt = 0, result = 0;
+
+int main()
+{
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	while (cin >> a)
+	{
+		if (a == 'i')
+			add_cnt++;
+		else
+			result += add_cnt;
+	}
+	cout << result;	
+}
